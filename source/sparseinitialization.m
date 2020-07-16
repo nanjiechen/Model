b7b02e5bcde6d761b95a58ca49ef2beb76572b18
nanjiechen <56348092+nanjiechen@users.noreply.github.com>
2020-07-16 19:50:38 -0400

    Delete sparseinitialization.m

--- source/sparseinitialization.m
+++
@@ -1,7 +0,0 @@
-function parameters = sparseinitialization(parameters)
-    parameters.stochasticmodel.N = parameters.GRFModel.numofeigenvalues;
-    parameters.stochasticmodel.width = 3;
-    parameters.stochasticmodel.level = 3;
-    methods.sparsegridinit = @sginit;
-    parameters = sginit(parameters);
-end
