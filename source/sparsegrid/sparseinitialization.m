1393b72d6bb9b55d5f51beb07834bdfd450bbf9e
nanjiechen <56348092+nanjiechen@users.noreply.github.com>
2020-09-11 10:50:47 -0400

    Add files via upload

--- source/sparsegrid/sparseinitialization.m
+++ source/sparsegrid/sparseinitialization.m
@@ -1,7 +1,7 @@
 function parameters = sparseinitialization(parameters)
     parameters.stochasticmodel.N = parameters.GRFModel.numofeigenvalues;
     parameters.stochasticmodel.width = 3;
-    parameters.stochasticmodel.level = 3;
+    parameters.stochasticmodel.level = 1;
     methods.sparsegridinit = @sginit;
     parameters = sginit(parameters);
 end
