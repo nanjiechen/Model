3143cd99b1c59901963e0ef3c2d200d8aae626fa
nanjiechen <56348092+nanjiechen@users.noreply.github.com>
2021-03-11 09:19:23 -0500

    Add files via upload

--- source/pipermfiles/testpiperonly.m
+++ source/pipermfiles/testpiperonly.m
@@ -10,7 +10,7 @@ cd piper
 loadlibrary('libMatPiper')
 
 matfri = 0;
-matlri = 0;
+matlri = 99;
 output = int16(0); % Flag false for do not output the data
 
 piperfile = 'piper_trypsin';
@@ -36,36 +36,36 @@ fprintf('Run piper with correct output energy vector: First test \n')
 output = int16(1); % Flag true for output of data
 matfri = 0; % First rotation
 %matlri = 0;   % Last rotation
-matlri = 100;
+matlri = 99;
 Energies = zeros(nx * ny * nz * (matlri - matfri + 1), 1,'single');
-
+piperfile = 'piper_trypsin_read';
 t1 = toc;
-[Energies,nx,ny,nz] = calllib('libMatPiper','MatPiper',Energies,1,1,1, matfri, matlri, output, piperfile);
+[Energies1,nx,ny,nz] = calllib('libMatPiper','MatPiper',Energies,1,1,1, matfri, matlri, output, piperfile);
 t2 = toc;
 fprintf('Total execution time = %f \n', t2 - t1)    
 fprintf('\n') 
 
 
 %%
-fprintf('Run piper with correct output energy vector: Second test \n')
+% fprintf('Run piper with correct output energy vector: Second test \n')
+% 
+% output = int16(1); % Flag true for output of data
+% matfri = 0; % First rotation
+% %matlri = 0;   % Last rotation
+% matlri2 = 19;
+% Energies2 = zeros(nx * ny * nz * (matlri2 - matfri + 1), 1,'single');
+% 
+% t1 = toc;
+% [Energies2,nx,ny,nz] = calllib('libMatPiper','MatPiper',Energies2,1,1,1, matfri, matlri2, output, piperfile);
+% t2 = toc;
+% fprintf('Total execution time = %f \n', t2 - t1)    
+% fprintf('\n') 
+% % en = norm(Energies2(1:length(Energies)) - Energies);
+% % fprintf('norm of difference = %f \n',en)
+% 
+% fprintf('matlri1 = %f matlri2= %f \n',matlri,matlri2)
 
-output = int16(1); % Flag true for output of data
-matfri = 0; % First rotation
-%matlri = 0;   % Last rotation
-matlri2 = 19;
-Energies2 = zeros(nx * ny * nz * (matlri2 - matfri + 1), 1,'single');
 
-t1 = toc;
-[Energies2,nx,ny,nz] = calllib('libMatPiper','MatPiper',Energies2,1,1,1, matfri, matlri2, output, piperfile);
-t2 = toc;
-fprintf('Total execution time = %f \n', t2 - t1)    
-fprintf('\n') 
-% en = norm(Energies2(1:length(Energies)) - Energies);
-% fprintf('norm of difference = %f \n',en)
-
-fprintf('matlri1 = %f matlri2= %f \n',matlri,matlri2)
-
-
-% unloadlibrary('libMatPiper')
+unloadlibrary('libMatPiper')
 
 cd ..
